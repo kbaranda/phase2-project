93e4246f515e6f10633ca80470f2a793695f5b01
Kat <bolivarkat@yahoo.com>
2021-12-04 22:57:26 -0600

    Created 3 client-side routes

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ClassesContainer from "./ClassesContainer";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Welcome to My Phase 2 Project!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/classes">Classes</Link>
+              </li>
+              <li>
+                <Link to="/about_us">About Us</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Switch>
+            <Route path="/classes">
+              <ClassesContainer />
+            </Route>
+            <Route path="/about_us">
+              <AboutUs />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
