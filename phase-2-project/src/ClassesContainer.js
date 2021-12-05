1c2f8c3207cad5c10473de77b780695b47d10e03
Kat <bolivarkat@yahoo.com>
2021-12-04 23:05:42 -0600

    added server with classes array

---
+++ phase-2-project/src/ClassesContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ClassesContainer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Classes</h2>
+      </div>
+    )
+  }
+}
+
+export default ClassesContainer;
---
+++ phase-2-project/src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Welcome to CPR Prime Services</h1>
+        <p>CPR Prime Services is your premier CPR, AED, and First-Aid certifier for The City of Eagle Pass, Tx. We are helping our community thrive with knowledgeable and confident individuals in our Certified CPR, AED, and First-Aid class taught by experienced certified instructors. We are also offering Basic Life Support Training to Healthcare Professionals.</p>
+      </div>
+    )
+  }
+}
+
+export default Home;
